5e4ce9b57ca84eb7e3dca85fc2f750fb110e9751
kakao <gywn6209@gmail.com>
2021-07-13 18:28:39 +0900

    minor change

--- app/src/main/java/com/example/egoeco_app/view/ObdDataDetailFragment.kt
+++ app/src/main/java/com/example/egoeco_app/view/ObdDataDetailFragment.kt
@@ -1,23 +1,17 @@
 package com.example.egoeco_app.view
 
-import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
-import com.example.egoeco_app.R
 import com.example.egoeco_app.databinding.FragmentObdDataDetailBinding
 import com.example.egoeco_app.viewmodel.ObdDataDetailViewModel
 import com.trello.rxlifecycle4.components.support.RxDialogFragment
-import com.trello.rxlifecycle4.components.support.RxFragment
 import dagger.hilt.android.AndroidEntryPoint
-import splitties.toast.toast
 import java.lang.IllegalStateException
 
 @AndroidEntryPoint
